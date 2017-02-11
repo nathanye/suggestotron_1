9b0b95360dd452ed16fe8c071698543c703ee622
nathanye <khalil54781@gmail.com>
2017-02-11 20:17:29 +0800

    add

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170211082552) do
+ActiveRecord::Schema.define(version: 20170211120138) do
+
+  create_table "abouts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "topics", force: :cascade do |t|
     t.string   "title"
